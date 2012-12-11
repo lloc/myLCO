d105ae300b2f7af1f1d776350b9aaa146098ef03
Dennis Ploetner <re@lloc.de>
2012-12-11 10:28:08 +0100

    Tabs vs. spaces

--- templates/page.php
+++ templates/page.php
@@ -7,26 +7,26 @@
 <script type="text/javascript">
 //<![CDATA[
 jQuery(document).ready( function($) {
-    $("img.set_pr").each(function() {
-        var loaderimg = $(this);
-        var data = {
-            action: "mylco_pagerank",
-            url: loaderimg.attr("alt")
-        };
-        jQuery.post(ajaxurl, data, function(response) {
-            loaderimg.hide().after(response);
-        });
-    });
-    $("img.set_alexa").each(function() {
-        var loaderimg = $(this);
-        var data = {
-            action: "mylco_alexa",
-            url: loaderimg.attr("alt")
-        };
-        jQuery.post(ajaxurl, data, function(response) {
-            loaderimg.hide().after(response);
-        });
-    });
+	$("img.set_pr").each(function() {
+		var loaderimg = $(this);
+		var data = {
+			action: "mylco_pagerank",
+			url: loaderimg.attr("alt")
+		};
+		jQuery.post(ajaxurl, data, function(response) {
+			loaderimg.hide().after(response);
+		});
+	});
+	$("img.set_alexa").each(function() {
+		var loaderimg = $(this);
+		var data = {
+			action: "mylco_alexa",
+			url: loaderimg.attr("alt")
+		};
+		jQuery.post(ajaxurl, data, function(response) {
+			loaderimg.hide().after(response);
+		});
+	});
 });
 //]]>
 </script>
